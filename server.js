16f2dde92db03ee958cbcbe4b09c829702477b11
Cyrus Shahrivar <cyrus.shahrivar@gmail.com>
2015-12-07 16:01:52 -0500

    brought in d3 example and my playground data querying into one file

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var app = express();
+var data = require('./data/2014_refugees.json');
 
-
+app.use(morgan('dev'));
 app.listen(3000, function () {
-  console.log("you are on port 3000");
+  console.log("working on port 3000");
+});
+app.use(express.static(__dirname+'/public'));
+
+app.get("/data", function (req,res) {
+  res.send(data);
 });
