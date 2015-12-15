ea3a10e3c80a114048c52b2141ec722ceac7c25e
Cyrus Shahrivar <cyrus.shahrivar@gmail.com>
2015-12-14 21:08:31 -0500

    took off need for database right now, going to merge with master as this is a functioning VERSION 1.0

--- server.js
+++ server.js
@@ -3,18 +3,18 @@ var morgan = require('morgan');
 var app = express();
 var data = require('./data/2014_refugees.json');
 var mongoose = require('mongoose');
-var refugees2014 = require('./models/2014.js');
+// var refugees2014 = require('./models/2014.js');
 // var sentiment = require('sentiment');
 
-mongoose.connect('mongodb://localhost/refugeeApp', function (err) {
-  if(err){
-    console.log(err);
-  } else {
-    console.log('connection successful - server');
-  }
-
-
-});
+// mongoose.connect('mongodb://localhost/refugeeApp', function (err) {
+//   if(err){
+//     console.log(err);
+//   } else {
+//     console.log('connection successful - server');
+//   }
+//
+//
+// });
 
 // var r1 = sentiment('OTTAWA — Canada welcomed 163 Syrian refugees under its new accelerated entry program late Thursday night, the first of 25,000 the country has promised to take in by March. FROM OUR ADVERTISERSPrime Minister Justin Trudeau and a battery of politicians from across the political spectrum were on hand at the Toronto airport to greet the refugees. “You are home,” Mr. Trudeau said to the first passengers to disembark after a 16-hour flight from Beirut on a Canadian military aircraft. “You’re safe at home now.” The premier of Ontario, Kathleen Wynne, gave them winter coats. Under a plan announced by Mr. Trudeau’s new government, a series of flights will bring 10,000 Syrian refugees into Canada by the end of this month and a total of at least 25,000 before March. The widespread embrace of the plan by the Canadian public stands in stark contrast with the controversy raging over the issue in the United States, where many politicians, especially on the right, have called for bans or restrictions on the admission of Syrian refugees.');
 // console.dir(r1);        //
@@ -29,12 +29,12 @@ app.get("/data", function (req,res) {
   res.send(data);
 });
 
-//getting same error with
-app.get("/2014data", function (req, res) {
-  refugees2014.find().exec(function (err, stuff) {
-    console.log(stuff);
-        console.log(err)
-    res.send(stuff);
-
-  });
-});
+// //getting same error with
+// app.get("/2014data", function (req, res) {
+//   refugees2014.find().exec(function (err, stuff) {
+//     console.log(stuff);
+//         console.log(err)
+//     res.send(stuff);
+//
+//   });
+// });
